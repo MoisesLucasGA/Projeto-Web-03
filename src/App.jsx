c3dc7aa5bfbf95785a5ffd68a2e2cb89c5cffe8b
MoisesLucasGA <mlucas.gondim@gmail.com>
2021-08-19 11:35:11 -0300

    Adicionando o Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "./components/header/Header"
 import LoginForm from "./components/login/LoginForm"
 // import CadastroForm from "./components/login/CadastroForm"
-// import Livro from "./components/livro/LivroIndex"
+import Livro from "./components/livro/LivroIndex"
 // import Autor from "./components/autor/AutorIndex"
 // import Editora from "./components/editora/EditoraIndex"
 
@@ -12,7 +12,7 @@ const App = () =>
     <>
         <div style={{display: "flex",flexDirection: "column",minHeight: "90vh"}}>
             <Header />
-            <LoginForm />    
+            <LoginForm />
         </div>
         <Footer />
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,21 @@
 import React from "react"
 import Twitter from "./LogoTwitter"
+import Instagram from "./LogoInstagram"
+import WhatsApp from "./LogoWhats"
+import Facebook from "./LogoFacebook"
 import './Footer.css'
 
 const Footer = () => 
     <footer className='footer'>
         <div className='social'>
             <Twitter />
-            <Twitter />
-            <Twitter />
-            <Twitter />
+            <Instagram />
+            <WhatsApp />
+            <Facebook />
         </div>
-        <div>
-            <p>Feito com ❤ por Moisés Lucas © 2021 Livros+</p>
+        <div className='creditos'>
+            <p>Feito com <span>❤</span> por Moisés Lucas © 2021 Livros+</p>
+            <p>Projeto III - Desenvolvimento de Software para Web 2021.1</p>
         </div>
     </footer>
 
